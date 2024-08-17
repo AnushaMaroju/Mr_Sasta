30029784cafb09e0da4c9f3d3c1972742e99d7f7
anusha <anusha.flyferry@gmail.com>
2024-08-17 22:13:15 +0530

    intial push

--- config/auth.js
+++ config/auth.js
@@ -4,10 +4,6 @@ apiKey = "5eb2dadf-858a-11eb-a9bc-0200cd936042",
 secreateKey = "Vecrep_secreate_key"
 
 
-
-
-
-
 module.exports={
     secreateKey,passWord,apiKey,email,region,bucketName,secretAccessKey,accessKeyId
 }
